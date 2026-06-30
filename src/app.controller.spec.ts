260c6badce6c8be3ba166457648bb7784c655d8b
Agus <vama.agus@gmail.com>
2026-06-30 00:06:01 -0600

    Modify a first unit and the e2e test.
    See them fail, adjust, see them turn green.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Personal Coach"', () => {
+      expect(appController.getHello()).toBe('Personal Coach');
     });
   });
 });
